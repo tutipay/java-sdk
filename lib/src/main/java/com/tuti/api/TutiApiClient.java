49166d8502e51b37ab9befe17a9e1e4227c9b807
Mohammed Salah <mseeltahir@gmail.com>
2022-10-19 15:46:31 +0200

    returned removed return statement

--- lib/src/main/java/com/tuti/api/TutiApiClient.java
+++ lib/src/main/java/com/tuti/api/TutiApiClient.java
@@ -99,6 +99,7 @@ public class TutiApiClient {
     private String getServerURL(boolean development) {
         String developmentHost = "https://staging.app.2t.sd/api/consumer/";
         String productionHost = "https://staging.app.2t.sd/api/consumer/";
+        return development ? developmentHost : productionHost;
     }
 
     public void SignIn(SignInRequest credentials, ResponseCallable<SignInResponse> onResponse, ErrorCallable<TutiResponse> onError) {

151bf34c30c88b182fa2f8bf2b5ffa068b9c7bb6
rami3sam <rami3sam@gmail.com>
2023-03-28 10:38:15 +0200

    fixing notifications after backend change

--- lib/src/main/java/com/tuti/Library.kt
+++ lib/src/main/java/com/tuti/Library.kt
@@ -7,6 +7,8 @@ import com.tuti.api.TutiApiClient
 import com.tuti.api.authentication.SignInRequest
 import com.tuti.api.authentication.SignInResponse
 import com.tuti.api.data.Card
+import com.tuti.model.Notification
+import com.tuti.model.NotificationFilters
 
 object Library {
     var jwt: String? = null
@@ -22,32 +24,27 @@ object Library {
         val uuid = "3c420386-5fd0-4b72-918a-5b87bba13f81"
 
         val card = Card(
-                PAN = tuti_card_pan,
-                expiryDate = tuti_card_exp_date
+            PAN = tuti_card_pan,
+            expiryDate = tuti_card_exp_date
         )
 
         client.SignIn(SignInRequest(
-                mobile = tuti_username,
-                password = tuti_password
+            mobile = tuti_username,
+            password = tuti_password
         ),
-                onResponse = { signInResponse: SignInResponse ->
-                    val token = signInResponse.authorizationJWT
-                    client.authToken = token
-                    client.getPaymentToken(uuid,
-                            onResponse = { PaymentToken ->
+            onResponse = { signInResponse: SignInResponse ->
+                val token = signInResponse.authorizationJWT
+                client.authToken = token
+                client.getNotifications(
+                    NotificationFilters("", true),
+                    onResponse = { notifications: List<Notification> ->
+                        println(notifications)
+                    },
+                    onError = { tutiResponse, exception -> }
+                )
 
-                            },
-                            onError = { paymentToken, exception ->
-                                exception?.let {
-
-                                }
-                                paymentToken?.let {
-                                    print("error occcurrrrrrrrrrrred")
-                                }
-                            })
-
-                },
-                onError = { tutiResponse, exception -> })
+            },
+            onError = { tutiResponse, exception -> })
     }
 }
 
